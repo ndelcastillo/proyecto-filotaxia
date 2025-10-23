b71b8224d4d0b9b276a0304c97a1a2cb8638a9d4
Nicolas del Castillo <delcastillonico95@gmail.com>
2025-10-23 00:54:45 -0300

    style: variables + typography

--- src/js/animations/gsap-textHover.js
+++ src/js/animations/gsap-textHover.js
@@ -10,16 +10,16 @@ document.querySelectorAll(".gallery__item").forEach(item => {
 
     item.addEventListener("mouseenter", () => {
         // Texto
-        gsap.to(original, { opacity: 0, y: -10, duration: 0.3, ease: "power2.out" });
-        gsap.to(alt, { opacity: 1, y: 0, duration: 0.3, ease: "power2.out" });
+        gsap.to(original, { opacity: 0, duration: 0, ease: "power2.out" });
+        gsap.to(alt, { opacity: 1, duration: 0, ease: "power2.out" });
 
         // Imagen - zoom dentro del wrapper (contenedor fijo)
         gsap.to(image, { scale: 1.03, duration: 1, ease: "power3.out" });
     });
 
     item.addEventListener("mouseleave", () => {
-        gsap.to(original, { opacity: 1, y: 0, duration: 0.3, ease: "power2.out" });
-        gsap.to(alt, { opacity: 0, y: 10, duration: 0.3, ease: "power2.out" });
+        gsap.to(original, { opacity: 1, duration: 0, ease: "power2.out" });
+        gsap.to(alt, { opacity: 0, duration: 0, ease: "power2.out" });
 
         gsap.to(image, { scale: 1, duration: 1, ease: "power3.out" });
     });
--- src/js/animations/gsap.js
+++ src/js/animations/gsap.js
@@ -1,9 +1,9 @@
 // Animaciones principales
+import './gsap-stepFlowScrollTrigger.js';
 import './gsap-imageHover.js';
 import './gsap-splitType.js';
 import './gsap-lenis.js';
 import './gsap-animationScrollTrigger.js';
-import './gsap-stepFlowScrollTrigger.js';
 import './gsap-transition.js';
 import './gsap-marquee.js';
 import './gsap-basics.js';
