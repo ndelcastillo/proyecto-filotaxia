652d3f5d764f5a6b3594103948b8cff589113915
Nicolas del Castillo <delcastillonico95@gmail.com>
2025-11-06 19:56:29 -0300

    style: responsive pagManifiesto

--- src/js/animations/gsap.js
+++ src/js/animations/gsap.js
@@ -3,6 +3,7 @@ import './gsap-stepFlowScrollTrigger.js';
 import './gsap-imageHover.js';
 import './gsap-splitType.js';
 import './gsap-lenis.js';
+import './gsap-horizontalScrollTrigger.js';
 import './gsap-animationScrollTrigger.js';
 import './gsap-transition.js';
 import './gsap-marquee.js';
